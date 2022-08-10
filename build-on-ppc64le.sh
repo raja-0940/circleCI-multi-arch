0fb39411694657dfb48a94079926cface1381e6a
Mayur Waghmode <42777188+mayurwaghmode@users.noreply.github.com>
2022-08-10 15:19:54 +0530

    Update build-on-ppc64le.sh

--- build-on-ppc64le.sh
+++ build-on-ppc64le.sh
@@ -1,11 +1,10 @@
-rm -rf /root/$GH_REPO
-git clone -b hosted-multi-arch https://github.com/mayurwaghmode/circleCI-multi-arch.git /root/$GH_REPO
-cd /root/$GH_REPO
-echo "Hello"
+sudo git clone -b hosted-multi-arch https://github.com/mayurwaghmode/circleCI-multi-arch.git /root/$GH_REPO
+sudo cd /root/$GH_REPO
+sudo echo "Hello"
 ARCH=$(uname -m)
 echo "world"
-echo "$QUAY_REPO"
-echo "$QUAY_USER"
+sudo echo "$QUAY_REPO"
+sudo echo "$QUAY_USER"
 sudo buildah bud -f Dockerfile -t "$QUAY_REPO":$ARCH .
 echo "$QUAY_PASS" | sudo buildah login -u "$QUAY_USER" --password-stdin quay.io
 sudo podman push "$QUAY_REPO":$ARCH
