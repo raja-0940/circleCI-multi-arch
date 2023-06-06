0fe882637248de08991022e5b2e230d056e0c84b
Rajakumar Battula <124244917+raja-0940@users.noreply.github.com>
2023-06-06 15:19:11 +0530

    Update build-on-ppc64le.sh

--- build-on-ppc64le.sh
+++ build-on-ppc64le.sh
@@ -1,7 +1,7 @@
 ARCH=$(uname -m)
 source circleci.env
 sudo rm -rf /root/$GH_REPO
-git clone -b hosted-multi-arch https://github.com/mayurwaghmode/circleCI-multi-arch.git /root/"$GH_REPO"
+git clone -b hosted-multi-arch https://github.com/raja-0940/circleCI-multi-arch.git /root/"$GH_REPO"
 cd /root/$GH_REPO
 buildah bud -f ppc64le/Dockerfile -t $QUAY_REPO:$ARCH .
 echo "$QUAY_PASS" | sudo buildah login -u "$QUAY_USER" --password-stdin quay.io
